e7717aa30ec23680f84a2639f1a5e8c90725b96b
velikhanov <velikhanov@users.noreply.github.com>
2023-03-21 21:30:42 +0400

    Initial commit

---
+++ about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include 'includes/head.php'; ?>
+<link rel="stylesheet" href="css/about.css">
+<title>About me</title>
+</head>
+<body>
+
+<?php include 'includes/header.php'; ?>
+
+<section class="section">
+   <h1 class="aboutme" data-phrase-id="Title">About me</h1>
+     <div class="about-p">
+       <h3 data-phrase-id="geninfotitle">General information</h3>
+       <p data-phrase-id="getinfotext">
+         My name is Teymur, I'm 22. I'm a web developer. I grew up and was born in Ukraine, the city of Zdolbunov, Rivne region. I have been living in Baku since 2014.
+       </p>
+       <h3 data-phrase-id="techinfotitle">Technical skills</h3>
+       <p data-phrase-id="techinfotext">
+         Experienced with HTML + CSS + jQuery + Bootsrap. Recently I switched to Bootsrap 5, which stopped supporting jQuery, so in my projects I also try to avoid it and write in pure JS.
+         I also work with php + Laravel. In general, I position myself as a Back End developer.
+         I can also disassemble/assemble computers and laptops, reinstall the OS, configure the OS, set up simple local networks.
+       </p>
+       <h3 data-phrase-id="personinfotitle">Personal qualities</h3>
+       <p data-phrase-id="personinfotext">
+         Active, purposeful, I quickly adapt to a new team. Responsible, I bring things to the end.
+       </p>
+       <h3 data-phrase-id="hobbyinfotitle">Hobby</h3>
+       <p data-phrase-id="hobbyinfotext">
+         I love sports, active lifestyle. Sometimes I play computer games.
+       </p>
+       <h3>CV</h3>
+       <a href="img/about/CV_Velikhanov_Teymur.pdf" target="_blank"><i class="fas fa-file"></i><span data-phrase-id="clickopen">Click to open</span></a>
+     </div>
+</section>
+<script src="js/languative.js" defer></script>
+<script src="lang/main.js" defer></script>
+<script src="lang/about.js" defer></script>
+<script src="js/scripts.js" async></script>
+</body>
+</html>
---
+++ lang/index.js
@@ -0,0 +1,1 @@
+languative.modifyDictionary("en",{Title:"Portfolio of Velikhanov Teymur"}),languative.modifyDictionary("az",{Title:"Velikhanov Teymurun portfolio",dev:"Developer"}),languative.modifyDictionary("ru",{Title:"Портфолио Велиханова Теймура",dev:"Разработчик"});
