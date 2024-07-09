0886cfe4e1e0e773b78bb3191c236544e102b67a
velikhanov <velikhanov@users.noreply.github.com>
2024-07-09 23:40:45 +0400

    Updated about.php,form.php,img/gears.png,includes/head.php,includes/header.php,index.html,index.php,skills.php,works.php

--- includes/head.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<link rel="shortcut icon" href="img/t.png" type="image/x-icon">
-<link rel="stylesheet" href="css/all.min.css">
-<link rel="stylesheet" href="css/main.css">
